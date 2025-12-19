816985403d9c901029f2223a6363163a455d3d03
Júnior <clebiojuniorcj@gmail.com>
2025-12-19 16:12:36 -0300

    chore: update runtests.jl file

---
+++ test/runtests.jl
@@ -0,0 +1,18 @@
+using MLJ
+using DataFrames
+using CategoryEncoder
+
+X = DataFrame(
+    gender  = ["M", "F", "M", missing],
+    Country = ["BR", "US", "BR", "FR"],
+    age     = [23, 35, 29, 41]
+)
+
+encoder = CategoryEncoder.SimpleLabelEncoder(:gender, :Country)
+mach = machine(encoder, X) |> fit!
+
+X_enc = CategoryEncoder.transform(mach, X)
+
+first(X_enc, 1)
+
+last(X_enc, 1)
